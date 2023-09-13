7c2058815f34fff8586f7814cd91f00caba6021d
Divasraj1 <Divasraj1>
2023-09-13 18:50:04 +0530

    project setup and created header

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import Services from "./Services";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    SpecialOffer,
+    Subscribe,
+    Services,
+    Footer,
+    CustomerReviews
+}
