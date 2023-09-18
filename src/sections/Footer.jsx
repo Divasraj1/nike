2e1c964026352707ad99cda4bfb685d8b796ff11
Divasraj1 <Divasraj1>
2023-09-18 18:43:43 +0530

    added footer section copyright name added

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -46,7 +46,7 @@ const Footer = () => {
       <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm: items-center">
             <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
               <img src={copyrightSign} alt="copyright sign" width={20} height={20} className="rounded-full m-0" />
-              <p>Copyright. All rights reserved.</p>
+              <p>Copyright. All rights reserved. Made with ❤ By Divasraj Vimal</p>
             </div>
             <p className="font-montserrat cursor-pointer">
               Terms & Conditions
